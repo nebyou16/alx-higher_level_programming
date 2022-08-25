d12d90aff20958a6a0af648155777c191971d7bd
nebyou16 <104837818+nebyou16@users.noreply.github.com>
2022-08-25 21:57:06 +0300

    Update 101-easy_print.py

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
-=======
-import printing_easy
->>>>>>> 96b768bfdec1c700b431ed01245443064e60230e
+__import__("sys").stdout.write("#pythoniscool\n")
