14975d52bb798af5b391cae70bb4038299b7e801
nebyou16 <104837818+nebyou16@users.noreply.github.com>
2022-08-25 22:01:42 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,30 +1,14 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-
 if __name__ == "__main__":
-    """Print the number of and list of arguments."""
     import sys
-
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
+    size = len(sys.argv)
+    if size-1 == 1:
+        print("{:d} argument".format(size - 1), end="")
     else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
-=======
-if __name__ == "__main__":
-    from sys import argv
-argc = len(argv)
-if argc < 2:
-    print("{} arguments.".format(argc - 1))
-else:
-    if argc == 2:
-        print("{} argument:".format(argc - 1))
+        print("{:d} arguments".format(size - 1), end="")
+    if size - 1 == 0:
+        print(".")
     else:
-        print("{} arguments:".format(argc - 1))
-    for n in range(1, argc):
-        print("{}: {}".format(n, argv[n]))
->>>>>>> 96b768bfdec1c700b431ed01245443064e60230e
+        print(":")
+    for i in range(1, size):
+        print("{:d}: {:s}".format(i, sys.argv[i]))
